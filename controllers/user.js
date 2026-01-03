36e64c5f660887ed9232c89fbca6562fade95464
Mohsin Javed <mohsinjavedpc@gmail.com>
2026-01-03 15:48:36 +0500

    user controller fix

--- controllers/user.js
+++ controllers/user.js
@@ -15,10 +15,12 @@ const postLogin = (req, res, next) => {
         if (doMatch) {
           return res.status(200).json({ message: "Login successful" });
         }
+        return res.status(401).json({ message: "Invalid password" });
       });
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json({ message: "An error occurred" });
     });
 };
 
@@ -29,23 +31,30 @@ const postSignup = (req, res, next) => {
   const confirmPassword = req.body.confirmPassword;
 
   User.findOne({ email: email }).then((user) => {
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    if (user) {
+      return res.status(409).json({ message: "User already exists" });
     }
-    if (userPassword !== confirmPassword) {
-      User.create({
-        name: name,
-        email: email,
-        password: userPassword,
-        role: "user",
-        isVarified: false,
-      })
-        .then((result) => {
-          return res.status(201).json({ message: "User created successfully" });
+    if (userPassword === confirmPassword) {
+      bcrypt.hash(userPassword, 12).then((hashedPassword) => {
+        User.create({
+          name: name,
+          email: email,
+          password: hashedPassword,
+          role: "user",
+          isVerified: false,
         })
-        .catch((err) => {
-          console.log(err);
-        });
+          .then((result) => {
+            return res
+              .status(201)
+              .json({ message: "User created successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: "Error creating user" });
+          });
+      });
+    } else {
+      return res.status(400).json({ message: "Passwords do not match" });
     }
   });
 };
