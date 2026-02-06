5c500975029c903ec9762d11fa61c97a401e5026
Mohsin Javed <mohsinjavedpc@gmail.com>
2026-02-06 22:58:52 +0500

    login + signup complete

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const userController = require("../controllers/user");
-
+const authToken = require("../auth/jwtAuthToken");
 const router = express.Router();
-
-// router.post("/login", userController, postLogin);
-
-// router.get("/signup", userController.getSignup);
 
 router.post("/login", userController.postLogin);
 
 router.post("/signup", userController.postSignup);
 
-// router.post("/logout", userController.postLogout);
+router.use(authToken);
+
+router.post("/logout", userController.postLogout);
 
 module.exports = router;
